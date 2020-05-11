8e20132443fdc7c9773da8dec6860735dfcba16f
felipejarar <felipe.jara.r@usach.cl>
2020-05-11 06:27:06 -0400

    Initial Package files

---
+++ Runtime/FileServerUtilities.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Motions.AssetBundles.FileServer
+{
+    public class FileServerUtilities : MonoBehaviour
+    {
+
+        public static string Version = "1.0.0";
+
+        public static string SomethingElse = "Something Else";
+
+    }
+
+}
