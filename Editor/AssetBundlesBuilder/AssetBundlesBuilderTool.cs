777ee2081847f06864da26c0786798d2cd40232f
felipejarar <felipe.jara.r@usach.cl>
2020-05-11 08:12:52 -0400

    Added Editor Folder

---
+++ Editor/AssetBundlesBuilder/AssetBundlesBuilderTool.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class AssetBundlesBuilderTool
+{
+    readonly static float _Window_minWidth  = 500;
+    readonly static float _Window_minHeight = 500;
+
+    [MenuItem("Assets/Build AssetBundles")]
+    static void init(){
+        AssetBundlesBuilderTools_Window window = ScriptableObject.CreateInstance<AssetBundlesBuilderTools_Window>();
+        window.position = new Rect(Screen.width / 6, Screen.height / 3, _Window_minWidth, _Window_minHeight);
+        window.minSize = new Vector2(_Window_minWidth, _Window_minHeight);
+        window.titleContent = new GUIContent("AssetBundles Builder Tool");
+        window.ShowAuxWindow();
+    }
+}
